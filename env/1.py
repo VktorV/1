3d0296fc386df9b8cab5b081a2e93a84b9e15a50
ViktorV <verkhorubovvv@mail.ru>
2024-02-05 02:59:11 +0800

    0502

--- env/1.py
+++ env/1.py
@@ -40,3 +40,190 @@
 #  print(res, end=" ")
 #  k+=1
 #  res= 2**k
+
+
+
+                            #3 задача1
+#Требуется вычислить, сколько раз встречается некоторое число k в массиве list_1.
+
+# import random
+# N = int(input('Введите размер массива N: '))
+# X = int(input('Введите число X: '))
+# N_array = []
+# for i in range(N):
+#     N_array.append(random.randint(0, N//2))
+# print(N_array)
+# print(f'Число вхождений посчитали с помощью встроенной функции count {N_array.count(X)}')
+
+
+
+                            # 3 задача2
+ # Требуется найти в массиве list_1 самый близкий по значению элемент к заданному числу k и вывести его.
+# n = int(input())
+# list_1 = list()
+# x = int(input())
+# for i in range(n):
+#     list_1.append(x)
+
+# k = int(input())
+# m = abs(k - list_1[0]) 
+
+# number = list_1[0]
+# for i in range(1, len(list_1)):
+#     if m > abs(list_1[i] - k):
+#         m = abs(list_1[i] - k)
+#         number = list_1[i]
+# print(number)
+
+
+                            # 3 задача2
+# Напишите программу, которая вычисляет стоимость введенного пользователем слова k и выводит его.
+# Будем считать, что на вход подается только одно слово, которое содержит либо только английские, либо только русские буквы.
+# dict_with_letter = {1: ("А","Е","В","И","Н","О","Р","С","Т", 
+#  'A','E','I','O','U','L','N','S','T','R'), 
+#  2: ("Д","К","Л","М","П","У", 
+#  'D', 'G'), 
+#  3: ("Б","Г","Ё","Ь","Я", 
+#  'B','C','M', 'P'), 
+#  4: ("Й","Ы", 
+#  'F', 'H', 'V', 'W', 'Y'), 
+#  5: ("Ж","З","Х","Ц","Ч", 
+#  'K'), 
+#  6: ("Ш", "Э","Ю", 
+#  'J','X'), 
+#  7: ("Ф","Щ","Ъ", 
+#  'Q','Z') 
+#  } 
+ 
+# def dicting(dict_with, words): 
+#  sum_points = 0 
+#  for key, value in dict_with.items(): 
+#   for letter in words.upper(): 
+#    if letter in value: 
+#     sum_points += key 
+#  print(sum_points) 
+# word = str(input()) 
+# dicting(dict_with_letter,word)
+                                     
+
+
+                                              #8. телефонная книга
+# '''1. Создание файла
+# - открываем файл на дозапись
+# 2. Внесение данных
+# - получить от пользователя контактные данные
+# - открываем файл на дозапись
+# - записываем данные
+# 3. Вывод данных на экран
+# - открываем файл на считывание
+# - получаем данные из файла
+# - выводим данные на экран
+# 4. Поиск контакта
+# - запрос варианта поиска контакта
+# - получить от пользователя данные для поиска
+# - открываем файл на считывание
+# - получаем данные из файла
+# - осуществляем поиск контакта
+# - выводим контакт на экран
+# 5. Создание интерфейса
+# - выводим меню пользователя на экран
+# - запрос действия
+# - запуск соответствующей функции
+# - осуществление выхода из программы'''
+
+def input_surname():
+    return input('Введите фамилию: ').title()
+
+def input_name():
+    return input('Введите имя: ').title()
+
+def input_patronymic():
+    return input('Введите отчество: ').title()
+
+def input_phone():
+    return input('Введите номер телефона: ')
+
+def input_address():
+    return input('Введите адрес (город): ').title()
+
+def create_contact():
+    surname = input_surname()
+    name = input_name()
+    patronymic = input_patronymic()
+    phone = input_phone()
+    address = input_address()
+    return f'{surname} {name} {patronymic} {phone}\n{address}'
+
+def data_input():
+    contact = create_contact()
+    with open('phonebook.txt', 'a', encoding='utf-8') as f:
+        f.write(f'{contact}\n\n')
+
+def read_file():
+    with open('phonebook.txt', 'r', encoding='utf-8') as f:
+     return f.read()
+
+def print_data():
+    print(read_file())
+
+
+def search_contact():
+    print(
+    'Варианты поиска:\n'\
+    '1 - по фамилии\n'\
+    '2 - по имени\n'\
+    '3 - по отчеству\n'\
+    '4 - по телефону\n'\
+    '5 - по адресу(городу)'
+    )
+    var = input('Выберите необходимый вариант: ')
+    while var not in ('1', '2', '3', '4', '5'):
+        print('Некорректный ввод данных!')
+        var = input('Выберите необходимый вариант: ')
+        print()
+    i_var = int(var) - 1
+
+    find = input('Введите данные для поиска: ').title()
+    print()
+    contacts = read_file()
+    contacts_list = contacts.strip().split('\n\n')
+    for contact_str in contacts_list:
+        contact_lst = contact_str.replace('\n', ' ').split()
+        # print(contact_lst)
+        if find in contact_lst[i_var]:
+         print(contact_str)
+    print()
+
+
+
+def interface():
+    with open("phonebook.txt", "a", encoding='utf-8'):
+        pass
+    choice = ''
+    while choice != '4':
+        print(
+            "Варианты действия: \n" \
+            "1 - Ввод данных контакта \n" \
+            "2 - Вывести данные на экран \n" \
+            "3 - Поиск контакта \n" \
+            "4 - Выход"
+            )
+        print()
+        choice = input("Выберите номер действия: ")
+        print()
+        while choice not in ('1', '2', '3', '4'):
+            print("Некорректный ввод данных!")
+            choice = input("Выберите номер действия: ")
+            print()
+        match choice:
+            case '1':
+             data_input()
+            case '2':
+             print_data()
+            case '3':
+                search_contact()
+            case '4':
+                print('Вс2его доброго!')
+
+if __name__ == '__main__':
+    interface()
